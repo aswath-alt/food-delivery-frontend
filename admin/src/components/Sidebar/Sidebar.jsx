c1e09d1323d78f036d15fa2f28fb26631b544f0d
aswath-alt <aswath7tech@gmail.com>
2026-07-21 08:57:48 +0530

    updated website

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
 import './Sidebar.css'
 import { assets } from '../../assets/assets'
+import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
-    <div className='sidebar'>
+    <div className="sidebar">
       <div className="sidebar-options">
-        <div className="sidebar-option">
-          <img src={assets.add_icon} alt="" />
-          <p>Add Items</p>
-        </div>
-      </div>
-      <div className="sidebar-option">
-          <img src={assets.order_icon} alt="" />
-          <p>List Items</p>
-        </div>
-        <div className="sidebar-option">
-          <img src={assets.order_icon} alt="" />
-          <p>Order</p>
-        </div>
 
+        <NavLink to="/add" className="sidebar-option">
+          <img src={assets.add_icon} alt="Add" />
+          <p>Add Items</p>
+        </NavLink>
+
+        <NavLink to="/list" className="sidebar-option">
+          <img src={assets.order_icon} alt="List" />
+          <p>List Items</p>
+        </NavLink>
+
+        <NavLink to="/orders" className="sidebar-option">
+          <img src={assets.order_icon} alt="Orders" />
+          <p>Orders</p>
+        </NavLink>
+
+      </div>
     </div>
   )
 }
--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,64 +1,100 @@
 import foodModel from "../models/foodModel.js";
-import fs from 'fs'
+import fs from "fs";
 
-// add fooditem
 
-const addFood = async(req,res)=>{
-    let image_filename = `${req.file.filename}`;
-
-    const food = new foodModel({
-        name:req.body.name,
-        description:req.body.description,
-        price:req.body.price,
-        category:req.body.category,
-        image:image_filename
-    })
-
+const addFood = async (req, res) => {
     try {
+        // safety check for file
+        if (!req.file) {
+            return res.json({
+                success: false,
+                message: "Image not uploaded"
+            });
+        }
+
+        const image_filename = req.file.filename;
+
+        const food = new foodModel({
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+            category: req.body.category,
+            image: image_filename
+        });
+
         await food.save();
-        res.json({succes:true,message:"Food Added"})
-        
+
+        res.json({
+            success: true,
+            message: "Food Added"
+        });
+
     } catch (error) {
         console.log(error);
-        res.json({succes:false,message:"Error"})
-        
-        
+
+        res.json({
+            success: false,
+            message: error.message
+        });
     }
+};
 
-}
 
-// add food list
 
-const listFood = async(req,res)=>{
+const listFood = async (req, res) => {
     try {
         const foods = await foodModel.find({});
-        res.json({succes:true,data:foods})
-        
+
+        res.json({
+            success: true,
+            data: foods
+        });
+
     } catch (error) {
         console.log(error);
-        res.json({succes:false,message:"Error"})
-        
-        
+
+        res.json({
+            success: false,
+            message: error.message
+        });
     }
+};
 
-}
 
-// remove food
-const removeFood = async(req,res)=>{
+
+const removeFood = async (req, res) => {
     try {
-        const food = await foodModel.findById(req.body.id)
-        fs.unlink(`uploads/${food.image}`,()=>{})
+        const food = await foodModel.findById(req.body.id);
 
-        await foodModel.findById(req.body.id);
-        res.json({succes:true,message:"Food removed"})
+        if (!food) {
+            return res.json({
+                success: false,
+                message: "Food not found"
+            });
+        }
+
+        // delete image file safely
+        fs.unlink(`uploads/${food.image}`, (err) => {
+            if (err) {
+                console.log("File delete error:", err);
+            }
+        });
+
+        await foodModel.findByIdAndDelete(req.body.id);
+
+        res.json({
+            success: true,
+            message: "Food removed"
+        });
+
     } catch (error) {
         console.log(error);
-        res.json({succes:false,message:"Error"})
-        
 
-        
+        res.json({
+            success: false,
+            message: error.message
+        });
     }
+};
 
-}
-
-export{addFood,listFood,removeFood}
+export { addFood, listFood, removeFood };
